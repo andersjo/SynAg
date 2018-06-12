d92d5efde4c655a859c3621c1551dc48265b1d99
JuliusSchwartz <39348336+JuliusSchwartz@users.noreply.github.com>
2018-06-12 12:56:00 +0100

    Update model2009.py

--- model2009.py
+++ model2009.py
@@ -8,25 +8,10 @@ from torch.nn.init import *
 from torch import optim
 from operator import itemgetter
 import numpy as np
+
 import utils2009
 import imp
-
-
-numberRegex = re.compile("[0-9]+|[0-9]+\\.[0-9]+|[0-9]+[0-9,]+")
-def normalize(word):
-    return 'NUM' if numberRegex.match(word) else word.lower()
-
-train_data = Path() # Path to training data
-dev_data = Path() # Path to development data
-    
-def extract_targets(sent, preds, roles):
-    target_tensor = torch.zeros(len(preds), len(sent))
-    for i in range(len(preds)):
-        for j in range(len(sent)):
-            target_tensor[i][j] = roles[sent[j][4][i]]
-    return torch.tensor(target_tensor, dtype=torch.long)
-    
-w2i, p2i, l2i, r2i, words, pos, lems = list(utils2009.vocab(train_data))
+imp.reload(utils2009)
 
 external_embedding_fp = open('../sskip.100.vectors', 'r')
 external_embedding_fp.readline()
@@ -34,12 +19,15 @@ external_embedding = {line.split(' ')[0]: [float(f) for f in line.strip().split(
                             external_embedding_fp}
 external_embedding_fp.close()
 
+train_data = Path() # Path to data
+dev_data = Path() # Path to data
+
+w2i, p2i, l2i, r2i, words, pos, lems = list(utils2009.vocab(train_data))
 train_sents = utils2009.extract_sent(train_data, external_embedding)
-
-
+role_list = list(r2i.keys())
 
 class SynAg(nn.Module):
-    def __init__(self, w2i, p2i, l2i, r2i, word_vocab, pos_vocab, lem_vocab):
+    def __init__(self, w2i, p2i, l2i, r2i, word_vocab, pos_vocab, lem_vocab, hidden_size, cell_size, num_layers):
         super().__init__()
         
         self.w2i = w2i
@@ -55,9 +43,13 @@ class SynAg(nn.Module):
         self.pos_emb = nn.Embedding(len(pos_vocab), 16)
         self.lem_emb = nn.Embedding(len(lem_vocab), 100)
         
-        self.arch = nn.LSTM(input_size=316, hidden_size = 512, num_layers = 1, bidirectional = True, batch_first= True )
+        self.hidden_size = hidden_size
+        self.cell_size = cell_size
+        self.num_layers = num_layers
         
-        self.cell_to_role = nn.Linear(2048, len(r2i))
+        self.arch = nn.LSTM(input_size=316, hidden_size = self.hidden_size, num_layers = self.num_layers, bidirectional = True, batch_first= True )
+        
+        self.cell_to_role = nn.Linear(4*self.hidden_size, len(self.r2i))
         
         
     def repn(self, sent, preds):
@@ -67,7 +59,8 @@ class SynAg(nn.Module):
         for pred in preds:
             n = 0
             for word in sent:
-                ran_word_emb = self.word_emb(torch.tensor(self.w2i[normalize(word[0][0])]))
+
+                ran_word_emb = self.word_emb(torch.tensor(self.w2i[utils2009.normalize(word[0][0])]))
             
                 if word[0][1] == 'KNOWN':
                     if word[0][0] == '.':
@@ -75,7 +68,7 @@ class SynAg(nn.Module):
                     else:
                         pre_emb = torch.tensor(external_embedding[word[0][0]])
                 else:
-                    pre_emb = self.word_emb(torch.tensor(self.w2i[normalize(word[0][0])]))
+                    pre_emb = self.word_emb(torch.tensor(self.w2i[utils2009.normalize(word[0][0])]))
             
                 if word[0][0] == pred[0][0]:
                     ran_lem_emb = self.lem_emb(torch.tensor(self.l2i[word[1]]))
@@ -98,13 +91,13 @@ class SynAg(nn.Module):
 
         sent_tensor, pred_idxs = self.repn(sent, preds)
         #print("shape of sent_tensor = ", sent_tensor.shape)        
-        self.hidden = torch.randn(2, len(preds), 512)
-        self.cell = torch.randn(2, len(preds), 512)           
+        self.hidden = torch.randn(2*self.num_layers, len(preds), self.hidden_size)
+        self.cell = torch.randn(2*self.num_layers, len(preds), self.cell_size)           
 
         lstm_out, (self.hidden, self.cell) = self.arch(sent_tensor, (self.hidden, self.cell))
         #print("lstm_output shape is ", lstm_out.shape)
        
-        pred_tensor = torch.randn(len(preds), len(sent), 2048)
+        pred_tensor = torch.randn(len(preds), len(sent), 4*self.hidden_size)
         for m in range(len(preds)):
             for n in range(len(sent)):
                 pred_tensor[m][n] = torch.cat((lstm_out[m][n], lstm_out[m][pred_idxs[m]]))
@@ -119,23 +112,29 @@ class SynAg(nn.Module):
         #print("role_scores shape is", role_scores.shape)
         #print("checkpoint")
 
-        return role_scores 
+        return role_scores #, torch.topk(scores, 1)[1]
         
         
-my_model = SynAg(w2i, p2i, l2i, r2i, words, pos, lems)        
+        
+my_model = SynAg(w2i, p2i, l2i, r2i, words, pos, lems, 512, 512, 1)
 loss_function = nn.NLLLoss()
 optimizer = optim.SGD(my_model.parameters(), lr=0.01)
 
-for sent in train_sents[0:100]:
-    sent_preds = [x for x in sent if x[3] == 'Y']
+for epoch in range(10):
+    print(epoch)
+    for sent in train_sents[0:50]:
+        sent_preds = [x for x in sent if x[3] == 'Y']
     
-    targs = extract_targets(sent, sent_preds, r2i)
+        targs = utils2009.extract_targets(sent, sent_preds, r2i)
     
-    my_model.zero_grad()
-    scores = my_model(sent, sent_preds)
-    n_scores = scores.permute(0, 2, 1)
+        my_model.zero_grad()
+        scores = my_model(sent, sent_preds)
+        n_scores = scores.permute(0, 2, 1)
     
-    loss = loss_function(n_scores, targs)
-    print(loss)
-    loss.backward()
-    optimizer.step()
+        loss = loss_function(n_scores, targs)
+        print(loss)
+        loss.backward()
+        optimizer.step()
+    
+    
+    
