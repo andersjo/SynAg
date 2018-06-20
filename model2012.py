04c2928e64fd87775e526e9e4e8c0f8f62eb2f40
JuliusSchwartz <39348336+JuliusSchwartz@users.noreply.github.com>
2018-06-20 20:31:13 +0100

    Update model2012.py

--- model2012.py
+++ model2012.py
@@ -1,29 +1,41 @@
 from collections import Counter
 import re
-numberRegex = re.compile("[0-9]+|[0-9]+\\.[0-9]+|[0-9]+[0-9,]+");
 import shutil
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.init import *
 from torch import optim
 from operator import itemgetter
-import numpy as np
-
 import imp
 import utils2012
-imp.reload(utils2012)
+import numpy as np
+import torch.nn.utils.rnn as rnn_utils
+import time
+import operator
 
+numberRegex = re.compile("[0-9]+|[0-9]+\\.[0-9]+|[0-9]+[0-9,]+");
+def normalize(word):
+    return 'NUM' if numberRegex.match(word) else word.lower()
 
-external_embedding_fp = open('../sskip.100.vectors', 'r')
+torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+external_embedding_fp = open('/homes/jds76/virtualenv/Project/data/sskip.100.vectors', 'r')
 external_embedding_fp.readline()
 external_embedding = {line.split(' ')[0]: [float(f) for f in line.strip().split(' ')[1:]] for line in
                             external_embedding_fp}
 external_embedding_fp.close()
 
-train_data = () # path to training data
+ext_train_data = '/homes/jds76/virtualenv/Project/data/2012_train.gold_conll'
+ext_dev_data = '/homes/jds76/virtualenv/Project/data/2012_train.gold_conll'
+ext_test_data = '/homes/jds76/virtualenv/Project/data/2012_test.gold_conll'
 
-w2i, p2i, l2i, r2i, words, pos, lems = list(utils2012.vocab(train_data))
-train_sentences = utils2012.extract_sent(train_data, external_embedding)
+train_data = '/homes/jds76/virtualenv/Project/data/conll-formatted-ontonotes-5.0/data/train/data/english/annotations/bc/cnn/00/cnn_0001.gold_conll'
+
+
+imp.reload(utils2012)
+w2i, p2i, l2i, r2i, words, pos, lems = list(utils2012.vocab(ext_train_data))
+train_sentences = utils2012.extract_sent(ext_train_data, external_embedding)
+dev_sentences = utils2012.extract_sent(ext_dev_data, external_embedding)
 role_list = list(r2i.keys())
 
 class SynAg(nn.Module):
@@ -39,9 +51,9 @@ class SynAg(nn.Module):
         self.pos_vocab = pos_vocab
         self.lem_vocab = lem_vocab
         
-        self.word_emb = nn.Embedding(len(word_vocab), 100)
-        self.pos_emb = nn.Embedding(len(pos_vocab), 16)
-        self.lem_emb = nn.Embedding(len(lem_vocab), 100)
+        self.word_emb = nn.Embedding(len(self.word_vocab), 100)
+        self.pos_emb = nn.Embedding(len(self.pos_vocab), 16)
+        self.lem_emb = nn.Embedding(len(self.lem_vocab), 100)
         
         self.hidden_size = hidden_size
         self.cell_size = cell_size
@@ -49,115 +61,193 @@ class SynAg(nn.Module):
         
         self.arch = nn.LSTM(input_size=316, hidden_size = self.hidden_size, num_layers = self.num_layers, bidirectional = True, batch_first= True )
         
-        self.cell_to_role = nn.Linear(4*self.hidden_size, len(r2i))
+        self.cell_to_role = nn.Linear(4*self.hidden_size, len(r2i), bias = False)
         
         
-    def repn(self, sent, preds):
-        m = 0
-        pred_idxs = []
-        rep_tensor = torch.randn(len(preds), len(sent), 316)
-        for pred in preds:
-            n = 0
-            for word in sent:
-                ran_word_emb = self.word_emb(torch.tensor(self.w2i[utils2012.normalize(word[0][0])]))
-            
-                if word[0][1] == 'KNOWN':
-                    if word[0][0] == '/.':
-                        pre_emb = torch.tensor(external_embedding['</s>'])
-                    else:
-                        pre_emb = torch.tensor(external_embedding[word[0][0]])
-                else:
-                    pre_emb = self.word_emb(torch.tensor(self.w2i[utils2012.normalize(word[0][0])]))
-            
-                if word[0][0] == pred[0][0]:
-                    ran_lem_emb = self.lem_emb(torch.tensor(self.l2i[word[1]]))
-                    pred_idxs.append(sent.index(word))
-                
-                else:
-                    ran_lem_emb = torch.zeros((100))
-                    
-                ran_pos_emb = self.pos_emb(torch.tensor(self.p2i[word[2]]))
-            
-                new_ent = torch.cat((ran_word_emb, pre_emb, ran_lem_emb, ran_pos_emb))
-                rep_tensor[m][n] = new_ent
 
-                n = n + 1
-            m = m + 1
-            
-        return rep_tensor, pred_idxs
+    def repn(self, sents):
+        count = 0
+        pred_count = 0
+        sent_count = 0
+
+        rep_dict = {}
+        rep_len = {}
+  
+        self.pred_idxs_dict = {}
     
-    def forward(self, sent, preds):
+        sent_order_dict = {}
+        sent_order_count = 0
+        
+        for sent in sents:         
+            sent_preds = [x for x in sent if 'V*' in x[3]]
+            
+            ord_pred_count = 0
+            
+            
+            for pred in sent_preds:
+                #rep_tensor = torch.randn(len(sent), 316)
+                rep_list = []
+                
+                
 
-        sent_tensor, pred_idxs = self.repn(sent, preds)
-        #print("shape of sent_tensor = ", sent_tensor.shape)        
-        self.hidden = torch.randn(2*self.num_layers, len(preds), self.hidden_size)
-        self.cell = torch.randn(2*self.num_layers, len(preds), self.cell_size)           
+                n = 0
+                for word in sent:
+                    if normalize(word[0][0]) in self.w2i:
+                        ran_word_emb = self.word_emb(torch.tensor(self.w2i[normalize(word[0][0])]))
+        
+                        if word[0][1] == 'KNOWN':
+                            if word[0][0] == '/.':
+                                pre_emb = torch.tensor(external_embedding['</s>'])
+                            else:
+                                pre_emb = torch.tensor(external_embedding[word[0][0]])
+                        else:
+                            pre_emb = self.word_emb(torch.tensor(self.w2i[normalize(word[0][0])]))
+            
+                    else:
+                        ran_word_emb = torch.randn(100)
+                        pre_word_emb = torch.randn(100)
+            
+                    if word[0][0] == pred[0][0]:
+                        if word[1] in self.l2i:
+                            ran_lem_emb = self.lem_emb(torch.tensor(self.l2i[word[1]]))
+                        else:
+                            ran_lem_emb = torch.randn(100)
+                        self.pred_idxs_dict[pred_count] = (sent.index(word), sent_count)
+                        pred_count += 1
+                        ord_pred_count +=1
+                
+                    else:
+                        ran_lem_emb = torch.zeros((100))
+                    
+                    ran_pos_emb = self.pos_emb(torch.tensor(self.p2i[word[2]]))
+            
+                    new_ent = torch.cat((ran_word_emb, pre_emb, ran_lem_emb, ran_pos_emb))
 
-        lstm_out, (self.hidden, self.cell) = self.arch(sent_tensor, (self.hidden, self.cell))
-        #print("lstm_output shape is ", lstm_out.shape)
+                    #rep_tensor[n] = new_ent
+                    rep_list.append(new_ent)
+
+                    n = n + 1
+                
+                rep_tensor = torch.stack([x for x in rep_list])
+                rep_dict[count] = rep_tensor
+
+                rep_len[count] = rep_tensor.shape[0]
+                sent_order_dict[count] = (sent_order_count, ord_pred_count-1)
+                count += 1
+                
+            sent_count +=1
+            sent_order_count +=1
+            
+        sorted_lengths = sorted(rep_len.items(), key=operator.itemgetter(1), reverse=True)
+        #print(sorted_lengths)
+        sorted_sentences = [rep_dict[x[0]] for x in sorted_lengths]
+        sent_lens = [x.shape[0] for x in sorted_sentences]
+        
+        #padded_seq = rnn_utils.pad_sequence(sorted_sentences, batch_first=True)
+        #packed_seq = rnn_utils.pack_padded_sequence(padded_seq, sent_lens, batch_first=True)
+        packed_seq = rnn_utils.pack_sequence(sorted_sentences)
+        
+        self.sorted_idxs = [self.pred_idxs_dict[x[0]] for x in sorted_lengths]
+        sent_order_list = [sent_order_dict[x[0]] for x in sorted_lengths]
+
+            
+        return packed_seq, sent_lens, sent_order_list
+    
+
+    def forward(self, sents):
+
+        packed_sequence, packed_lengths, order_list = self.repn(sents)     
+        
+        self.hidden = torch.randn(2*self.num_layers, len(packed_lengths), self.hidden_size)
+        self.cell = torch.randn(2*self.num_layers, len(packed_lengths), self.cell_size) 
+        
+        lstm_out, (self.hidden, self.cell) = self.arch(packed_sequence, (self.hidden, self.cell))
+        lstm_out = rnn_utils.pad_packed_sequence(lstm_out)[0]
+        lstm_out = lstm_out.permute(1, 0, 2)
        
-        pred_tensor = torch.randn(len(preds), len(sent), 4*self.hidden_size)
-        for m in range(len(preds)):
-            for n in range(len(sent)):
-                pred_tensor[m][n] = torch.cat((lstm_out[m][n], lstm_out[m][pred_idxs[m]]))
-        
-        #print("shape of pred_tensor", pred_tensor.shape)
-
-        
+        #pred_tensor = torch.zeros(len(packed_lengths), max(packed_lengths), 4*self.hidden_size)
+        #print("pred_tensor shape is ", pred_tensor.shape)
+        col_list = []
+        for m in range(len(packed_lengths)):
+            row_list = []
+            for n in range(max(packed_lengths)):
+                if torch.sum(lstm_out[m][n]) != 0:
+                    #pred_tensor[m][n] = torch.cat((lstm_out[m][n], lstm_out[m][self.sorted_idxs[m][0]]))
+                    row_list.append(torch.cat((lstm_out[m][n], lstm_out[m][self.sorted_idxs[m][0]])))
+                else:
+                    row_list.append(torch.zeros(4*self.hidden_size))
+            row_tensor = torch.stack([x for x in row_list])
+            #print("row_tensor shape is ", row_tensor.shape)
+            col_list.append(row_tensor)
+        pred_tensor = torch.stack([x for x in col_list])
+        #print("col_tensor shape is", col_tensor.shape)
+                
         role_space = self.cell_to_role(pred_tensor)
-        #print("role_space shape is", role_space.shape)
-
         role_scores = F.log_softmax(role_space, dim = 2)
+        
+        #print("lstm_output shape is ", lstm_out.shape)
+        #print("shape of pred_tensor", pred_tensor.shape)
+        #print("role_space shape is", role_space.shape)
         #print("role_scores shape is", role_scores.shape)
         #print("checkpoint")
 
-        return role_scores
+        #return role_scores
+        #return role_space
+        return role_scores, packed_lengths, order_list#, self.sorted_idxs
+
+      
+
+def get_targets(sent, roles, pred_num):
+    target = []
+    for i in range(len(sent)):
+        target.append(roles[sent[i][3][pred_num]])
+
+    return torch.tensor(target)
+      
         
-        
-my_model = SynAg(w2i, p2i, l2i, r2i, words, pos, lems, 512, 512, 1)
+my_model = SynAg(w2i, p2i, l2i, r2i, words, pos, lems, 512, 512, 4)
+my_model.cuda()
+
+
+
 loss_function = nn.NLLLoss()
-optimizer = optim.SGD(my_model.parameters(), lr=0.01)
 
 
-for epoch in range(10):
-    print(epoch)
-    for sent in train_sentences[0:50]:
 
-        sent_preds = [x for x in sent if x[3][0] == 'V*']
-
+for k in range(1):
     
-        targs = utils2012.extract_targets(sent, sent_preds, r2i)
-    
-        optimizer.zero_grad()
-        scores = my_model(sent, sent_preds)
-        n_scores = scores.permute(0, 2, 1)
-    
-        loss = loss_function(n_scores, targs)
-        print(loss)
-        loss.backward()
-        optimizer.step()
+    start = time.time()
 
-
-#### Below is my attempt at predicting role labels
-correct = 0
-total = 0
-
-for sent in train_sentences[50:100]:
-    preds = [x for x in sent if x[3][0] == 'V*']
-    targs = utils2012.extract_targets(sent, preds, r2i)
+    #batch = train_sentences[10*k:10*(k+1)]
+    batch = train_sentences[:50]
+    scores, p_lengths, order_list = my_model(batch)
     
-    with torch.no_grad():
-        tag_scores = my_model(sent, preds)
+    print(time.time() - start)
+    
+    print(len(p_lengths))
+
+    sen_list = []
+    for i in range(len(p_lengths)):
+        sen_list.append(scores[i][:p_lengths[i]])
         
 
+    j = 0        
+        
+    for x in order_list:
+        targets = get_targets(batch[x[0]], r2i, x[1])
+        #print(targets.shape)
+        loss = loss_function(sen_list[j], targets)
+        j+=1
     
-    for i in range(tag_scores.shape[0]):
-        for j in range(tag_scores.shape[1]):
-            total += 1
-            val, idx = tag_scores[i][j].max(0)
-            #print(role_list[idx])
-            if idx == targs[i][j]:
-                correct +=1
-                
-print("accuracy =", (100.0*correct)/total)
+
+
+
+
+
+
+
+
+
+
+
 
